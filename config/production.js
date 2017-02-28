b112c35313c323d4c52ea04cc2606c0cca2d7cee
liuwensa <liu1028hmily@163.com>
2017-02-28 15:10:25 +0800

    增加prodution.js配置

---
+++ config/production.js
@@ -0,0 +1,13 @@
+/**
+ * Created by admin on 2017/2/28.
+ */
+
+'use strict';
+
+module.exports = {
+  log: {
+    logFileDir    : '/raid/logs/admin',
+    needConsole   : false,
+    replaceConsole: false
+  }
+};
